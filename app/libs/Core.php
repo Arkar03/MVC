d337ef10164d1d5485a08e4b31c6b70b5e1f7ee7
Arkar03 <arkarmin.mi@gmail.com>
2023-06-06 17:32:18 +0630

    MVC

---
+++ app/libs/Core.php
@@ -0,0 +1,34 @@
+<?php
+require_once "../app/bootstrap.php";
+class Core
+{
+    private $className = "Home";
+    private $methodName = "index";
+    private $params = [];
+
+    public function __construct()
+    {
+        $this->getUrl();
+    }
+    public function getUrl()
+    {
+        $url = isset($_GET['url']) ? rtrim($_GET['url'], '/') : '';
+
+        $url = explode("/", $url);
+
+        if (!empty($url[0])) {
+            if (file_exists(ucfirst($url[0]) . ".php")) {
+                $className = $url[0];
+            }
+        }
+        require_once ucfirst($className) . ".php";
+        $className = new $className();
+
+        if (!empty($url[1])) {
+            if (method_exists($className, $url[1])) {
+                $methodName = $url[1];
+                unset($url[1]);
+            }
+        }
+    }
+}
--- post.php
+++ post.php
@@ -1,3 +1,19 @@
 <?php 
 
-echo "I am post.php";
+class Post {
+    public function __construct()
+    {
+        echo "I am constructor of ". __CLASS__ . " class<br>";
+    }
+
+    public function index()
+    {
+        echo "I am index method of " . __CLASS__ . " class<br>";
+    }
+
+    public function show($data = [])
+    {
+        echo "I have nothing to show<br>";
+        echo print_r($data);
+    }
+}
